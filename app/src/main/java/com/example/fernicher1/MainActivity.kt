edee6cdf47590132720cf06c60c3e4459ade67ef
shlomo <shlomo.gevim@gmail.com>
2020-04-25 21:37:58 +0300

    After Adding piano in the room

--- app/src/main/java/com/example/fernicher1/MainActivity.kt
+++ app/src/main/java/com/example/fernicher1/MainActivity.kt
@@ -1,32 +1,66 @@
 package com.example.fernicher1
 
+import android.graphics.Color.RED
+import android.graphics.Color.WHITE
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
+import android.widget.Button
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.collision.Box
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.ViewRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.CompletableFuture
 
 private const val PEEK = 50f
 
 class MainActivity : AppCompatActivity() {
-     private val models= mutableListOf(
-         Model(R.drawable.chair,"Chair",R.raw.chair),
-         Model(R.drawable.oven,"Oven",R.raw.oven),
-         Model(R.drawable.piano,"Piano",R.raw.piano),
-         Model(R.drawable.table,"Table",R.raw.table)
-     )
+    private val models = mutableListOf(
+        Model(R.drawable.chair, "Chair", R.raw.chair),
+        Model(R.drawable.oven, "Oven", R.raw.oven),
+        Model(R.drawable.piano, "Piano", R.raw.piano),
+        Model(R.drawable.table, "Table", R.raw.table)
+    )
+    lateinit var arFragment: ArFragment
+    lateinit var selectedModel: Model
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        arFragment=fragment as ArFragment
         setupButtomSheet()
         setupRecyclerView()
+        setupDoubleTapArPlaneListner()
+    }
+
+    private fun setupDoubleTapArPlaneListner(){
+        arFragment.setOnTapArPlaneListener { hitResult, _, _t ->
+            loadModel { modelRenderable, viewRenderable ->
+                addNodeToSence(hitResult.createAnchor(),modelRenderable,viewRenderable)
+            }
+        }
+
     }
 
     private fun setupRecyclerView() {
         rvModels.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        rvModels.adapter = ModelAdapter(models)
+        rvModels.adapter = ModelAdapter(models).apply {
+            selectedModl.observe(this@MainActivity, Observer {
+                this@MainActivity.selectedModel = it
+                val newTitle = "Models (${it.title})"
+                tvModel.text = newTitle
+            })
+        }
     }
 
 
@@ -36,5 +70,66 @@ class MainActivity : AppCompatActivity() {
             TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP, PEEK, resources.displayMetrics
             ).toInt()
+
+    }
+
+    private fun getCurrentScene()=arFragment.arSceneView.scene
+
+    private fun addNodeToSence(               // locate the object anh his view in the ARsence
+        anchor: Anchor,                        // anchore point, fill the object is part of the scene
+        modelRenderable: ModelRenderable,
+        viewRenderable: ViewRenderable
+    ) {
+        val anchorNode=AnchorNode(anchor)         //anchor node is the parent node
+        val modelNode=TransformableNode(arFragment.transformationSystem).apply {
+            renderable=modelRenderable
+            setParent(anchorNode)                      // case that his parents will be anchor node
+            getCurrentScene().addChild(anchorNode)
+            select()
+        }
+        val viewNode=Node().apply {      // its include the delete buttom
+            renderable=null
+            setParent(modelNode)           // his parent is modelNode
+            val box=modelNode.renderable?.collisionShape as Box
+            localPosition= Vector3(0f,box.size.y,0f)  // it value of x,y,z locale cordinate mesure from anchor node
+            (viewRenderable.view as Button).setOnClickListener {  // in a case when you press delete buttom
+                getCurrentScene().removeChild(anchorNode)
+            }
+        }
+      modelNode.setOnTapListener{_,_->
+        if (!modelNode.isTransforming){
+        if (viewNode.renderable==null){
+            viewNode.renderable=viewRenderable
+        }else{
+            viewNode.renderable=null
+        }
+        }
+      }
+
+    }
+
+    private fun createDeleteButton(): Button {
+        return Button(this).apply {
+            text = "Delete"
+            setBackgroundColor(RED)
+            setTextColor(WHITE)
+        }
+    }
+
+    private fun loadModel(callback: (ModelRenderable, ViewRenderable) -> Unit) { // the fun get callback from herself
+        val modelRenderable = ModelRenderable.builder()
+            .setSource(this, selectedModel.modelResourceId)  //appear in the raw directory, creat the object
+            .build()
+        val viewRenderable = ViewRenderable.builder()
+            .setView(this, createDeleteButton())            //add delete buttom to the object
+            .build()
+        CompletableFuture.allOf(modelRenderable, viewRenderable)
+            .thenAccept {
+                callback(modelRenderable.get(), viewRenderable.get())
+            }
+            .exceptionally {
+                Toast.makeText(this, "Some Error->${it.message}", Toast.LENGTH_LONG).show()
+                null
+            }
     }
 }
--- app/src/main/java/com/example/fernicher1/ModelAdapter.kt
+++ app/src/main/java/com/example/fernicher1/ModelAdapter.kt
@@ -1,29 +1,54 @@
 package com.example.fernicher1
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_model.view.*
 
-class ModelAdapter (val models:List<Model>):RecyclerView.Adapter<ModelAdapter.ModelViewHolder>() {
+const val SELECTED_MODEL_COLOR = Color.YELLOW
+const val UNSELECTED_MODEL_COLOR = Color.LTGRAY
+
+class ModelAdapter(val models: List<Model>) : RecyclerView.Adapter<ModelAdapter.ModelViewHolder>() {
 
 
-
+    var selectedModl = MutableLiveData<Model>()
+    private var selectedModelIndex = 0
+    private var modelViewHolders = mutableListOf<ModelViewHolder>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModelViewHolder {
-        val inflator=LayoutInflater.from(parent.context)
-        val view=inflator.inflate(R.layout.item_model,parent,false)
+        val inflator = LayoutInflater.from(parent.context)
+        val view = inflator.inflate(R.layout.item_model, parent, false)
         return ModelViewHolder(view)
     }
 
-    override fun getItemCount()=models.size
+    override fun getItemCount() = models.size
 
     override fun onBindViewHolder(holder: ModelViewHolder, position: Int) {
-       holder.view.apply {
-           ivThmbait.setImageResource(models[position].imageResourceId)
-           tvTitle.text=models[position].title
-       }
+        if (!modelViewHolders.contains(holder)) {
+            modelViewHolders.add(holder)
+        }
+        holder.view.apply {
+            ivThmbait.setImageResource(models[position].imageResourceId)
+            tvTitle.text = models[position].title
+            if (selectedModelIndex==position){
+                setBackgroundColor(SELECTED_MODEL_COLOR)
+                selectedModl.value=models[position]
+            }
+            setOnClickListener {
+                selectModelPosition(position)
+            }
+        }
     }
-    class ModelViewHolder(val view: View):RecyclerView.ViewHolder(view)
+
+    private fun selectModelPosition(position: Int) {
+        modelViewHolders[selectedModelIndex].view.setBackgroundColor(UNSELECTED_MODEL_COLOR)
+        selectedModelIndex = position
+        modelViewHolders[selectedModelIndex].view.setBackgroundColor(SELECTED_MODEL_COLOR)
+        selectedModl.value = models[selectedModelIndex]
+    }
+
+    class ModelViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 }
